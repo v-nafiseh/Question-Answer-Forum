887b8a63e4dfa6bd6e614bd74a22d73896ffb972
v-nafiseh <nafisehvalizadeh1@gmail.com>
2020-12-11 00:58:51 +0330

    added staticfiles_urlpatterns which checks whether we are in debug mode or not and if we are then adds to urlpatterns

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('questions/', include('questions.urls')),
     path('accounts/', include('accounts.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns 
